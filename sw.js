6e47fd8a7bfb78f6b9026d610fcf25a8e39618d7
Alexandre Dametto <168128352+AlexWan17@users.noreply.github.com>
2026-06-27 09:43:36 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "daniela-v2";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./favicon.svg",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
